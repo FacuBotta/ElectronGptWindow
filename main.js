f610cb2baebeaee63a8b9719755bcdac14ca1038
facubotta <nutuestampados@gmail.com>
2024-08-21 17:40:07 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Deshabilitar la aceleración de hardware
+app.disableHardwareAcceleration();
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+      webSecurity: true,
+      experimentalFeatures: false
+    }
+  });
+      
+  
+    
+  // Establecer el User-Agent personalizado
+  mainWindow.webContents.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
+
+  // mainWindow.loadFile('index.html');
+  mainWindow.loadURL('https://chat.openai.com');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
